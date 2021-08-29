1138174d68e8feed47348b09e663cf9b2e855a96
Andrew White <white-aj@outlook.com>
2021-08-29 22:52:47 +0100

    Add black, flake8, isort configs

--- poshdate/formatter.py
+++ poshdate/formatter.py
@@ -1,11 +1,10 @@
-from datetime import datetime
-from datetime import date
+from datetime import date, datetime
 from typing import Union
 
 
 def from_datetime(date_obj: Union[datetime, date]) -> str:
     date_ = int(date_obj.strftime("%d"))
-    
+
     check_date = date_ if (date_ < 20) else (date_ % 10)
     if check_date == 1:
         suffix = "st"
@@ -15,5 +14,5 @@ def from_datetime(date_obj: Union[datetime, date]) -> str:
         suffix = "rd"
     else:
         suffix = "th"
-    
+
     return f"{date_}{suffix} {date_obj:%B %Y}"
