86d089b61c78e755fb78ec55461420f9d6983622
Andrew White <white-aj@outlook.com>
2021-08-30 22:17:03 +0100

    Fix failing test
    add isinstance guard clause for datatypes

--- poshdate/formatter.py
+++ poshdate/formatter.py
@@ -3,6 +3,10 @@ from typing import Union
 
 
 def from_datetime(date_obj: Union[datetime, date]) -> str:
+    if not isinstance(date_obj, (datetime, date)):
+        raise TypeError(
+            f"{from_datetime.__name__} expects a datetime or date object, perhaps you enterd an int or str ?"
+        )
     date_ = int(date_obj.strftime("%d"))
 
     check_date = date_ if (date_ < 20) else (date_ % 10)
